73606844bfdc4f6834c27775f0ea246f82125e93
hugohutri <55588133+hugohutri@users.noreply.github.com>
2022-10-18 18:27:09 +0300

    init

---
+++ App.tsx
@@ -0,0 +1,33 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * Generated with the TypeScript template
+ * https://github.com/react-native-community/react-native-template-typescript
+ *
+ * @format
+ */
+
+import React from 'react';
+import {StatusBar, StyleSheet, Text, View} from 'react-native';
+
+const App = () => {
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+      {/* <Translated /> */}
+      <StatusBar backgroundColor="#49a" />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#49a',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
+export default App;
